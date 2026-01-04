f9b1f8d1a08eb7602518bad4a5116fde35610d95
betin <betinvoker1797@gmail.com>
2026-01-04 16:04:07 +0300

    Fix avtorization for app barista and update page accept_order

--- coffeshop/config/urls.py
+++ coffeshop/config/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('orderPanel/', include('barista_app.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 # Только в DEBUG-режиме!
